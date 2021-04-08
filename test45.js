6f1fbab7729be18fe1f553d8a7d493abfd50ad6f
Carl Leitner <litlfred@gmail.com>
2021-04-08 14:59:48 -0400

    Clearer output

--- test45.js
+++ test45.js
@@ -12,7 +12,7 @@ const CONTENT = {
   "b": "2005-10-04",
   "d": "h1dZ75FMdY9EQQjE64O4",
   "p": "bMlJkAt0V92RYhhG3fNt",
-  "s": "SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+  "s": "SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJVadQssw5c",
   "k": "12345678"
 }
 
@@ -20,33 +20,29 @@ let str = JSON.stringify(CONTENT)
 console.log("Looking at JSON string: " + str)
 console.log("  Length: " + str.length)
 
-let condStr = str.replaceAll('"','').replaceAll('{','').replaceAll('}','')
-console.log("Condesnsed string: " + condStr)
-console.log("  Length: " + condStr.length)
 
-let encoded = cbor.encode(CONTENT)
 
-console.log("CBOR encodded string: " + encoded)
-console.log("  Length: " + encoded.length)
+console.log ("\n\n\JSON String Encodings")
 
-let b45str = base45.encode(encoded)
-
-console.log("Based 45 Encoded of CBOR: " + b45str)
-console.log("  Length: " + b45str.length)
-
-console.log("JSON String / Q")
+console.log("JSON String / L")
 qrcode.toString(str, { errorCorrectionLevel: 'L' }, (err,string) => {
   console.log(string)
 } )
-console.log(qrcode.create(condStr, { errorCorrectionLevel: 'L' }))
+console.log(qrcode.create(str, { errorCorrectionLevel: 'L' }))
 qrcode.toFile('json_l.png', [ {data: str}], { errorCorrectionLevel: 'L' }, (err,out) => {})
 
 console.log("JSON String / Q")
 qrcode.toString(str, { errorCorrectionLevel: 'Q' }, (err,string) => {
   console.log(string)
 } )
-console.log(qrcode.create(condStr, { errorCorrectionLevel: 'Q' }))
+console.log(qrcode.create(str, { errorCorrectionLevel: 'Q' }))
 qrcode.toFile('json_q.png', [ {data: str}], { errorCorrectionLevel: 'Q' }, (err,out) => {})
+
+
+console.log ("\n\n\Condensend String Encodings")
+let condStr = str.replaceAll('"','').replaceAll('{','').replaceAll('}','')
+console.log("Condesnsed string: " + condStr)
+console.log("  Length: " + condStr.length)
 
 console.log("Condensed String / L")
 qrcode.toString(condStr, { errorCorrectionLevel: 'L' }, (err,string) => {
@@ -61,6 +57,21 @@ qrcode.toString(condStr, { errorCorrectionLevel: 'Q' }, (err,string) => {
 } )
 console.log(qrcode.create(condStr, { errorCorrectionLevel: 'Q' }))
 qrcode.toFile('cond_q.png', [ {data: condStr}], { errorCorrectionLevel: 'Q' }, (err,out) => {})
+
+console.log ("\n\n\nBase 45 Encodings")
+
+
+let encoded = cbor.encode(CONTENT)
+
+console.log("CBOR encodded string: " + encoded)
+console.log("  Length: " + encoded.length)
+
+let b45str = base45.encode(encoded)
+
+console.log("Based 45 Encoded of CBOR: " + b45str)
+console.log("  Length: " + b45str.length)
+
+
 
 console.log("b45 String / L")
 qrcode.toString(b45str, { errorCorrectionLevel: 'L' }, (err,string) => {
