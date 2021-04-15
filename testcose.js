194df77c5be28b322394c204b797499c8950b027
= <=>
2021-04-15 14:51:46 -0600

    Added documentation for creating hex keys for COSE in testcose.js

--- testcose.js
+++ testcose.js
@@ -2,6 +2,7 @@ const cose = require('cose-js')
 const fs = require('fs')
 const cbor = require('cbor')
 const base32 = require('base32')
+const base45 = require('base45')
 const qrcode = require('qrcode')
 /*
  * for ES256 create keys like this:
@@ -11,6 +12,16 @@ const qrcode = require('qrcode')
  * for ES512 create keys like this:
  * ssh-keygen -t ecdsa -b 521 -m PEM -f ./ecdsa521.pem
  * ssh-keygen -e -m PKCS8 -f ./ecdsa521.pem.pub > ecdsa521.pub
+ *
+ *
+ * To get HEX versions you can do:
+ * openssl ec -in /PATH/TO/ecdsa256.pem -text -noout
+ *
+ * It will display the private and public keys as hex.
+ * private key can be used as signer.key.d below (remove ':')
+ *
+ * For the verifier, remove the first bit and split in half for 
+ * verifier.key.x and verifier.key.y
  */
 const ALGO = 'ES256'
 
@@ -35,8 +46,13 @@ const headers = {
 const signer = {
   key: {
     'd': Buffer.from('6c1382765aec5358f117733d281c1c7bdc39884d04a45a1e6c67c858bc206c19', 'hex')
+
   }
 }
+console.log('KEY',signer.key.d.toString('hex'))
+console.log('KEY',signer.key.d)
+console.log('KEY',signer.key.d.toString())
+console.log("END KEY")
 
 const verifier = {
   key: {
@@ -48,11 +64,13 @@ const verifier = {
 cose.sign.create(headers, JSON.stringify(DATA), signer).then ( (buf) => {
   console.log(buf.toString('hex'))
   let buf_32 = base32.encode(buf).toUpperCase()
+  let buf_45 = base45.encode(buf)
   console.log(qrcode.create(buf.toString('hex'), { errorCorrectionLevel: 'Q' }))
-  console.log(qrcode.create(buf_32, { errorCorrectionLevel: 'Q' }))
+  console.log(qrcode.create(buf_45, { errorCorrectionLevel: 'Q' }))
 
   console.log(buf)
-  let rebuf = Buffer.from(base32.decode( buf_32 ), 'binary')
+  //let rebuf = Buffer.from(base32.decode( buf_32 ), 'binary')
+  let rebuf = Buffer.from(base45.decode( buf_45 ), 'binary')
   console.log(rebuf)
 
   cose.sign.verify( rebuf, verifier ).then( (ver) => {
